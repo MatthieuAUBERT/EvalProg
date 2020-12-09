e785a3bb90db1a6676870d20967ffefb8d258bf7
MatthieuAUBERT <74655021+MatthieuAUBERT@users.noreply.github.com>
2020-12-09 10:30:42 +0100

    Update Exercice1.py

--- Exercice1.py
+++ Exercice1.py
@@ -7,12 +7,16 @@ listeMot = [ "clamer" , "inhale" , "eclair" , "tourne" , "voiler" , "fourmi", "c
 Mot = random.choice(listeMot)
 print (Mot)
 
-playerInput = input("Saisir votre mot de 6 lettres")
-
-for i in range (0,6):
-    if Mot[i] == playerInput[i]:
-        print ( i , "Bon" )
-    else:
-        for j in range (0,6):
-            if Mot[i] == playerInput[j]:
-                print (j , "Mauvais endroit")
+for Essais in range (0,8):
+    playerInput = input("Saisir votre mot de 6 lettres")
+    for i in range (0,6):
+        if Mot[i] == playerInput[i]:
+            print ( i , "Bon" )
+        else:
+            for j in range (0,6):
+                if Mot[i] == playerInput[j]:
+                    print (j , "Mauvais endroit" , "Devrait être en " , i )
+    if Mot == playerInput :
+        print ("Vous avez gagné")
+        exit()
+print ("Vous avez perdu")
