9657c721ca623ba94a81970c49a154d086367dcc
MatthieuAUBERT <74655021+MatthieuAUBERT@users.noreply.github.com>
2020-12-09 10:19:22 +0100

    Update Exercice1.py

--- Exercice1.py
+++ Exercice1.py
@@ -3,7 +3,7 @@ from colorama import init
 init()
 from coloramaimport Fore, Back, Style
 
-listeMot = [ "clamer" , "inhale" , "eclair" , "tourne" , "voiler" , "fourmi", "comble", "inclus" , "chante" , "retard" ]
+listeMot = [ "clamer" , "inhale" , "eclair" , "tourne" , "voiler" , "fourmi", "comble", "inclus" , "chante" , "contre" ]
 Mot = random.choice(listeMot)
 print (Mot)
 
@@ -12,5 +12,7 @@ playerInput = input("Saisir votre mot de 6 lettres")
 for i in range (0,6):
     if Mot[i] == playerInput[i]:
         print ( i , "Bon" )
-    else :
-        print ( i , "Faux")
+    else:
+        for j in range (0,6):
+            if Mot[i] == playerInput[j]:
+                print (j , "Mauvais endroit")
