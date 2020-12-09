d3de5acf2088ccaa67e706bc6a7d3d60f8a5497f
MatthieuAUBERT <74655021+MatthieuAUBERT@users.noreply.github.com>
2020-12-09 12:08:43 +0100

    Update Exercice1.py

--- Exercice1.py
+++ Exercice1.py
@@ -1,5 +1,6 @@
 import random
 import string
+import time
 from colorama import init
 init()
 from colorama import Fore, Back, Style
@@ -7,12 +8,14 @@ from colorama import Fore, Back, Style
 def compteur(Mot):
     alphabet = list(string.ascii_lowercase)
     compteur = []
+    listlettre = []
     compte = 0
     for i in range (0,len(alphabet)):
         compte = Mot.count(alphabet[i])
         if compte !=0 :
             compteur.append(compte)
-    return compteur
+            listlettre.append(alphabet[i])
+    return compteur,listlettre
 
 
 listeMot = [ "clamer" , "inhale" , "eclair" , "tourne" , "voiler" , "fourmi", "comble", "inclus" , "chante" , "contre" ]
@@ -20,21 +23,21 @@ Mot = random.choice(listeMot)
 print (Mot)
 
 for Essais in range (0,8):
-    playerInput = input("Saisir votre mot de 6 lettres")
+    playerInput = input(Back.RESET + "Saisir votre mot de 6 lettres   ")
     for i in range (0,6):
         if Mot[i] == playerInput[i]:
-            print ( Back.RED + playerInput[i] )
+            print ( Back.RED + playerInput[i], end =' ' )
         else:
             color = False
             for j in range (0,6):
                 if Mot[i] == playerInput[j]:
-                    print ( Back.YELLOW + playerInput[i])
+                    print ( Back.YELLOW + playerInput[i], end =' ')
                     color = True
             if color == False :
-                print ( Back.BLUE + playerInput[i] )                
+                print ( Back.BLUE + playerInput[i] , end =' ')
+    print ("")            
     if Mot == playerInput :
-        print (Back.RED + playerInput)
-        print ("Vous avez gagné")
+        print (Back.RESET + "Vous avez gagné")
+        time.sleep(3)
         exit()
 print ("Vous avez perdu")
-
