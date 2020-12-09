daa41d65844ef6c4da81b57d5f1646104b4afb09
MatthieuAUBERT <74655021+MatthieuAUBERT@users.noreply.github.com>
2020-12-09 11:40:44 +0100

    Update Exercice1.py

--- Exercice1.py
+++ Exercice1.py
@@ -1,12 +1,23 @@
 import random
+import string
 from colorama import init
 init()
 from colorama import Fore, Back, Style
 
+def compteur(Mot):
+    alphabet = list(string.ascii_lowercase)
+    compteur = []
+    compte = 0
+    for i in range (0,len(alphabet)):
+        compte = Mot.count(alphabet[i])
+        if compte !=0 :
+            compteur.append(compte)
+    return compteur
+
+
 listeMot = [ "clamer" , "inhale" , "eclair" , "tourne" , "voiler" , "fourmi", "comble", "inclus" , "chante" , "contre" ]
 Mot = random.choice(listeMot)
 print (Mot)
-
 
 for Essais in range (0,8):
     playerInput = input("Saisir votre mot de 6 lettres")
@@ -22,6 +33,8 @@ for Essais in range (0,8):
             if color == False :
                 print ( Back.BLUE + playerInput[j] )                
     if Mot == playerInput :
+        print (Back.RED + playerInput)
         print ("Vous avez gagné")
         exit()
 print ("Vous avez perdu")
+
