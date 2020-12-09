b8de28cdcc3b01ec5f086e5ca73efab575fe1ac8
MatthieuAUBERT <74655021+MatthieuAUBERT@users.noreply.github.com>
2020-12-09 10:03:16 +0100

    Update Exercice1.py

--- Exercice1.py
+++ Exercice1.py
@@ -6,3 +6,11 @@ from coloramaimport Fore, Back, Style
 listeMot = [ "clamer" , "inhale" , "eclair" , "tourne" , "voiler" , "fourmi", "comble", "inclus" , "chante" , "retard" ]
 Mot = random.choice(listeMot)
 print (Mot)
+
+playerInput = input("Saisir votre mot de 6 lettres")
+
+for i in range (0,6):
+    if Mot[i] == playerInput[i]:
+        print ( i , "Bon" )
+    else :
+        print ( i , "Faux")
