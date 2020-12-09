569019ed654f454e982caeb7cdb7a36706648922
MatthieuAUBERT <74655021+MatthieuAUBERT@users.noreply.github.com>
2020-12-09 10:41:23 +0100

    Update Exercice1.py

--- Exercice1.py
+++ Exercice1.py
@@ -1,21 +1,26 @@
 import random
 from colorama import init
 init()
-from coloramaimport Fore, Back, Style
+from colorama import Fore, Back, Style
 
 listeMot = [ "clamer" , "inhale" , "eclair" , "tourne" , "voiler" , "fourmi", "comble", "inclus" , "chante" , "contre" ]
 Mot = random.choice(listeMot)
 print (Mot)
 
+
 for Essais in range (0,8):
     playerInput = input("Saisir votre mot de 6 lettres")
     for i in range (0,6):
         if Mot[i] == playerInput[i]:
-            print ( i , "Bon" )
+            print ( Back.RED + Mot[i] )
         else:
+            color = False
             for j in range (0,6):
                 if Mot[i] == playerInput[j]:
-                    print (j , "Mauvais endroit" , "Devrait être en " , i )
+                    print ( Back.YELLOW + Mot[i])
+                    color = True
+            if color == False :
+                print ( Back.BLUE + Mot[i] )                
     if Mot == playerInput :
         print ("Vous avez gagné")
         exit()
